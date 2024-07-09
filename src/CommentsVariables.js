fc41a28dfbe5dbddcac8fb6ea03b9fb785ebc445
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-08 17:18:24 -0700

    update

---
+++ src/CommentsVariables.js
@@ -0,0 +1,27 @@
+var number = 5; // in-line comment
+
+/* this is a 
+dddd
+d
+
+s
+s
+df
+
+df
+multi-line comment */
+
+number = 9; 
+
+/* Data Types: 
+undefined, 
+null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Beau";
+
+myName = 8;
+
+let ourName = "freeCodeCamp"; // defined within its scope
+
+const pi = 3.14; // stays the same throughout the program
