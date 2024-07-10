a48ec892ecc8f0122d20b0525d6b6fb098451c92
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-10 16:58:32 -0700

    modifying arrays

--- src/Operations.js
+++ src/Operations.js
@@ -100,3 +100,30 @@ var lastLetter = firstName[firstName.length - 1]
 console.log(lastLetter)
 
 // Store values in arrays
+var ourArray = ["John", 23];
+
+var myArray = ["Quincy", 1]
+console.log(myArray)
+
+// nested arrays
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = [["Bulls", 23], ["White Sox", 45]]
+
+// access array elements
+var ourArray = [50,60,70]
+var ourData = ourArray[0]
+
+//setup
+var myArray = [50,60,70]
+
+var myData = myArray[0]
+console.log(myData)
+
+//modify array 
+var ourArray = [18,64,99];
+ourArray[1] = 45;
+
+//setup
+var myArray = [18,64,99];
+myArray[0] = 45;
