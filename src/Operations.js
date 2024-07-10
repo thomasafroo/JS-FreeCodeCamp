0c59c02640102970789bfbf49006edf327517b23
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-09 20:17:23 -0700

    more operations

--- src/Operations.js
+++ src/Operations.js
@@ -18,3 +18,28 @@ myVar++;
 
 // Decrementing numbers
 myVar--;
+
+
+// floating point
+var ourDecimal = 5.7;
+
+var myDecimal = 0.0009;
+
+// multiply decimals
+var product = 2.0 * 2.5;
+console.log(product);
+
+//divide decimals
+var quotient = 4.4 / 2.0;
+
+//finding a remainder
+var remainder;
+remainder = 11 % 3;
+
+// strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+
+
+var myStr = "This is the start. " + "This is the end."
+console.log(myStr)
