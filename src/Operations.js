97fedc3370643c8bae784675970895dd55ddf849
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-10 17:01:59 -0700

    accessing elements in multi-dimensional arrays

--- src/Operations.js
+++ src/Operations.js
@@ -127,3 +127,10 @@ ourArray[1] = 45;
 //setup
 var myArray = [18,64,99];
 myArray[0] = 45;
+
+//access multi-dimensional arrays with indices
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13 ,14]];
+
+var myData = myArray[0][0];
+console.log(myData)
+console.log(myArray[2][1])
