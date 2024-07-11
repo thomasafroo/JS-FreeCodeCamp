fd8ea6ea5032d4502c505625b75c9f3b497d6b97
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-10 17:17:23 -0700

    push and pop to arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -33,3 +33,14 @@ var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13 ,14]];
 var myData = myArray[0][0];
 console.log(myData)
 console.log(myArray[2][1])
+
+// manipulate arrays with push()
+var ourArray = ["Stimpson", "J", "cat"]
+ourArray.push(["happy", "joy"])
+// ourArray = ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+//setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// only change code below this line
+myArray.push(["dog"])
