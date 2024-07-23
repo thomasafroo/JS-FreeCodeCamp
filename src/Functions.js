02ad3ea7f12a93d2840dbb8a6b506a6164f751ea
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-22 23:27:54 -0700

    printing local and global variables

--- src/Functions.js
+++ src/Functions.js
@@ -1,51 +1,50 @@
 // Example
 
 function ourReusableFunction() {
-    console.log("Heyya, World");
+  console.log("Heyya, World");
 }
 
-ourReusableFunction();  
-
+ourReusableFunction();
 
 function reusableFunction() {
-    console.log("Hi world");
+  console.log("Hi world");
 }
 
 reusableFunction();
 
 // passing values as arguments
 function ourFunctionWithArgs(a, b) {
-    console.log(a - b);
+  console.log(a - b);
 }
 ourFunctionWithArgs(10, 5); // outputs 5
 
 // Only change code below this line.
 function functionWithArgs(a, b) {
-    console.log(a + b);
+  console.log(a + b);
 }
 
-functionWithArgs(10, 5)
+functionWithArgs(10, 5);
 
 // scope
 
 var myGlobal = 10;
 
 function fun1() {
-    // assign 5 to oopsGlobal here
-    oopsGlobal = 5; // since there is no 'var' it automatically becomes global
+  // assign 5 to oopsGlobal here
+  oopsGlobal = 5; // since there is no 'var' it automatically becomes global
 }
 
 // only change code above this line
 function fun2() {
-    var output = "";
-    var output = "";
-    if (typeof myGlobal != "undefined") {
-        output += "myGlobal: " + myGlobal;
-    }
-    if (typeof oopsGlobal != "undefined") {
-        output += " oopsGlobal: " + oopsGlobal;
-    }
-    console.log(output);
+  var output = "";
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
 }
 
 fun1();
@@ -53,8 +52,8 @@ fun2();
 
 //local scope and functions
 function myLocalScope() {
-    var myVar = 5;
-    console.log(myVar);
+  var myVar = 5;
+  console.log(myVar);
 }
 
 myLocalScope();
@@ -63,9 +62,10 @@ myLocalScope();
 var outerWear = "T-shirt";
 
 function myOutfit() {
+  var outerWear = "sweater";
 
-
-    return outerWear;
+  return outerWear;
 }
 
 console.log(myOutfit());
+console.log(outerWear);
