197acb3b35960f18727bdaae99164868f73ef895
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-24 17:49:19 -0700

    new function

--- src/Functions.js
+++ src/Functions.js
@@ -94,3 +94,15 @@ function addFive() {
 }
 
 // assignment with a returned value
+var changed = 0;
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
