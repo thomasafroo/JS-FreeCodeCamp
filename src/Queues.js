ddab50a8afed7181518f529af153bf292e48c9ab
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-24 18:52:39 -0700

    queues

--- src/Queues.js
+++ src/Queues.js
@@ -1,3 +1,10 @@
 function nextLine(arr, item) {
     
+    return item;
 }
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
