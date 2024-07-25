ddefdeb836e1ed7998203e20590d96ca3c9574bd
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-24 17:46:54 -0700

    calling a function inside of a print statement

--- src/Functions.js
+++ src/Functions.js
@@ -71,3 +71,15 @@ console.log(myOutfit());
 console.log(outerWear);
 
 // return a value from a Function with Return 
+function minusSeven(num) {
+  return num - 7;
+}
+
+console.log(minusSeven(10));
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
+
