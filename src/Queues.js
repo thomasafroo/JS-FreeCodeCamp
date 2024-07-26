a59635c8f2d4a5ef2ddf2172a6b4a6a2cdaf84e3
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-25 18:37:56 -0700

    booleans

--- src/Queues.js
+++ src/Queues.js
@@ -8,3 +8,5 @@ var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+// boolean values
