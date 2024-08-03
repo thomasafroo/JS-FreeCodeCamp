ff343d58374bc88c65f6665c8fa51a6b78413411
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-02 23:33:47 -0700

    test else

--- src/ConditionalLogic.js
+++ src/ConditionalLogic.js
@@ -137,3 +137,17 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(10);
+
+// test else
+function testElse(val) {
+    var result = "";
+    // only change code below this line
+
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or Smaller";
+    }
+
+    return result;
+}
